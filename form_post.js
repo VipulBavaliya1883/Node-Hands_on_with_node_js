8dac73025e081596358687aef903987728892bb7
VipulBavaliya1883 <vbavaliya719@rku.ac.in>
2022-01-06 21:42:10 +0530

    all is well!!!

--- form_post.js
+++ form_post.js
@@ -3,12 +3,12 @@ var querystring = require('querystring');
 
 var server = http.createServer(function(req,res){
     var data1 = "";
-    res.on('data',function(chunk){
-        console.log(chunk.tostring());
+    req.on('data',function(chunk){
+        console.log(chunk.toString().String);
         data1 +=chunk; 
     });
 
-    res.on('end',function(){
+    req.on('end',function(){
     var uname = querystring.parse(data1)["uname"];
     var branch = querystring.parse(data1)["branch"];
 
